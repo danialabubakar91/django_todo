ed626733b758013f6abefca34b43567cb5c5a055
Danial <danialabubakar91@gmail.com>
2022-03-31 16:45:23 +0800

    Added api app

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from todo import views
 
 urlpatterns = [
@@ -23,6 +23,7 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signupuser'),
     path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+
     #Todos
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
@@ -31,4 +32,7 @@ urlpatterns = [
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
+    #API
+    path('api/', include('api.urls')),
 ]
